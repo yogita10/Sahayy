f598843be21167c92bbe5a0cc183559764c992b8
yogita <yogita10khurana@gmail.com>
2021-09-03 22:55:56 +0530

    update

---
+++ login_form2/models/User.js
@@ -0,0 +1,47 @@
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  text:{
+    type: String,
+    required: true
+  },
+  address:{
+    type:String 
+  },
+  donate:{
+    type:String,
+    required:true
+  },
+  quantity:{
+    type:Number, 
+    required:true 
+  },
+  adhar:{
+    type:Number, 
+    required:true 
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+
+ 
+  
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
